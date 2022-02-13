555cf5bd059f04bd6ba9bdc7f8f01f851641743b
Subhan khan <98511230+SubhanXKhan@users.noreply.github.com>
2022-02-14 01:29:15 +0500

    Update Pathan-302.py

--- Pathan-302.py
+++ Pathan-302.py
@@ -1855,8 +1855,7 @@ def results():
     global token
     cek_ok = open("Subhanok.txt","r").read()
     cek_cp = open("Subhancp.txt","r").read()
-    print("\n %s[%s1%s] NAAM TO SONA HOGA [SUBHAN]"
-    print("\n %s[%s1%s] Result OK\n %s[%s2%s] Result CP\n %s[%s0%s] Back to menu"%(p,k,p,p,k,p,p,k,p"))
+    print("\n %s[%s1%s] Result OK\n %s[%s2%s] Result CP\n %s[%s0%s] Back to menu"%(p,k,p,p,k,p,p,k,p))
     cek_ress = raw_input("\n \033[4;33mChoose%s:\x1b[0m\n %s➤%s➤%s➤%s➤ %s"%(p,m,k,h,b,p"))
     if cek_ress in ["1","01"]:
         if len(cek_ok) != 0:
